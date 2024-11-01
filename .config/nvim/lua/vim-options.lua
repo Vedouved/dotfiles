9247d6a1a27b78d71b183e2404164df88be806af
Vedouved <vedouved@gmail.com>
2024-11-02 00:11:35 +0400

    yay

---
+++ .config/nvim/lua/vim-options.lua
@@ -0,0 +1,16 @@
+vim.cmd("set expandtab")
+vim.cmd("set number")
+vim.cmd("set autoindent")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set smarttab")
+vim.cmd("set softtabstop=4")
+vim.cmd("set mouse=a")
+
+vim.cmd("highlight Normal guibg=none")
+vim.cmd("highlight NonText guibg=none")
+vim.cmd("highlight Normal ctermbg=none")
+vim.cmd("highlight NonText ctermbg=none")
+
+vim.g.mapleader= " "
+
