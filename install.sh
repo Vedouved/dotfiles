9a4ae1ab1644a6da577fa1f6999f0ebca99216ba
Vedouved <55759685+Vedouved@users.noreply.github.com>
2024-11-24 01:57:46 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,5 +23,5 @@ cp ~/dotfiles/.p10k.sh ~/
 cp ~/dotfiles/.viminfo ~/
 cp ~/dotfiles/.zshrc ~/
 sudo cp -r ~/dotfiles/fonts/fonts /usr/share/fonts
-sudo cp -r ~/dotfiles/ly /etc
+sudo cp -r ~/dotfiles/ly /etc/
 source ~/.zshrc
