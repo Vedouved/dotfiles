db8b28a9abf9aa8cca152d027244720bd14e5ba6
Vedouved <55759685+Vedouved@users.noreply.github.com>
2024-12-09 20:53:18 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,14 +11,6 @@ sleep 10
 
 paru -S waypaper pywal-git
 
-#install oh-my-zsh
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-
-#install zsh themes and addons
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 #installing rice
 cp -r ~/dotfiles/.config ~/
 cp ~/dotfiles/.p10k.sh ~/
