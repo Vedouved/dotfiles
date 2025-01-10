84e77333ca50c8838899a3916281e1ebe38049d6
Vedouved <55759685+Vedouved@users.noreply.github.com>
2025-01-10 23:54:44 +0400

    Update ohmyzsh-installer.sh

--- ohmyzsh-installer.sh
+++ ohmyzsh-installer.sh
@@ -5,3 +5,7 @@ git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#install rc files
+cp ~/dotfiles/.p10k.sh ~/
+cp ~/dotfiles/.zshrc ~/
