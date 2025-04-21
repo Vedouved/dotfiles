f7823d1209a7a8acad14019e207cd496e3f30684
Vedouved <55759685+Vedouved@users.noreply.github.com>
2025-04-21 19:45:42 +0400

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
--- Set the leader key to Left Control (Ctrl)
-vim.g.mapleader = "<C-space>"  -- Left Control + Space as the leader key
+-- Set the leader key to Space
+vim.g.mapleader = " "
 
 -- Bootstrap Lazy.nvim if not installed
 local install_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -7,6 +7,7 @@ if not vim.loop.fs_stat(install_path) then
   vim.fn.system({
     "git",
     "clone",
+    "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     install_path,
   })
@@ -17,15 +18,15 @@ vim.opt.rtp:prepend(install_path)
 require("lazy").setup({
   -- LSP Configuration
   {
-    "neovim/nvim-lspconfig",          -- LSP Config
+    "neovim/nvim-lspconfig",
     dependencies = {
-      "williamboman/mason.nvim",       -- Mason for LSP installation
-      "williamboman/mason-lspconfig.nvim", -- Mason LSP config
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
     },
     config = function()
       require("mason").setup()
       require("mason-lspconfig").setup()
-      
+
       -- LSP settings
       local lspconfig = require("lspconfig")
       local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -37,29 +38,28 @@ require("lazy").setup({
       })
 
       -- TypeScript Language Server (typescript-language-server)
-      lspconfig.ts_ls.setup({
+      lspconfig.tsserver.setup({
         capabilities = capabilities,
         on_attach = function(client, bufnr)
-          -- Disable formatting for ts_ls because typescript-language-server handles it
           client.resolved_capabilities.document_formatting = false
           client.resolved_capabilities.document_range_formatting = false
         end,
       })
 
       -- Add other LSP servers as needed
-    end
+    end,
   },
 
   -- Autocompletion
   {
-    "hrsh7th/nvim-cmp",          -- Completion plugin
+    "hrsh7th/nvim-cmp",
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",    -- LSP completions
-      "hrsh7th/cmp-buffer",      -- Buffer completions
-      "hrsh7th/cmp-path",        -- Path completions
-      "hrsh7th/cmp-cmdline",     -- Command line completions
-      "L3MON4D3/LuaSnip",        -- Snippets
-      "saadparwaiz1/cmp_luasnip",-- LuaSnip for nvim-cmp
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
     },
     config = function()
       local cmp = require("cmp")
@@ -83,75 +83,75 @@ require("lazy").setup({
           { name = "luasnip" },
         },
       })
-    end
+    end,
   },
 
   -- Syntax Highlighting
   {
-    "nvim-treesitter/nvim-treesitter",    -- Treesitter for syntax highlighting
+    "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = "all",            -- Install all languages
-        highlight = { enable = true },       -- Enable syntax highlighting
-        indent = { enable = true },          -- Enable automatic indentation
+        ensure_installed = { "python", "lua", "typescript", "javascript", "bash", "json", "yaml" },
+        highlight = { enable = true },
+        indent = { enable = true },
       })
-    end
+    end,
   },
 
-  -- Catpuccin Colorscheme
+  -- Catppuccin Colorscheme
   {
-    "catppuccin/nvim",                   -- Catpuccin theme
+    "catppuccin/nvim",
     config = function()
-      vim.cmd("colorscheme catppuccin-macchiato")  -- Set macchiato variant of Catpuccin
-    end
+      vim.cmd("colorscheme catppuccin-macchiato")
+    end,
   },
 
   -- File explorer (nvim-tree)
   {
-    "nvim-tree/nvim-tree.lua",           -- File tree
+    "nvim-tree/nvim-tree.lua",
     config = function()
       require("nvim-tree").setup({
         view = {
-          width = 30,                     -- Set the width of the file tree
-          side = "left",                  -- Position the file tree on the left
-          number = true,                  -- Show line numbers in the file tree
-          relativenumber = true,          -- Show relative line numbers in the file tree
+          width = 30,
+          side = "left",
+          number = true,
+          relativenumber = true,
         },
         filters = {
-          dotfiles = false,              -- Show dotfiles
+          dotfiles = false,
         },
         renderer = {
-          group_empty = true,            -- Group empty directories
-          highlight_opened_files = "name", -- Highlight opened files in the tree
+          group_empty = true,
+          highlight_opened_files = "name",
         },
       })
-    end
+    end,
   },
 
   -- Statusline
   {
-    "nvim-lualine/lualine.nvim",         -- Statusline
+    "nvim-lualine/lualine.nvim",
     config = function()
       require("lualine").setup({
         options = {
-          theme = "catppuccin",  -- Use Catpuccin theme for the status line
+          theme = "catppuccin",
         },
       })
-    end
+    end,
   },
 
   -- Git integration
   {
-    "lewis6991/gitsigns.nvim",           -- Git integration in Neovim
+    "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
-    end
+    end,
   },
 
   -- Fuzzy Finder
   {
-    "nvim-telescope/telescope.nvim",     -- Fuzzy finder
+    "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
@@ -164,76 +164,237 @@ require("lazy").setup({
           },
         },
       })
-    end
+    end,
   },
 
   -- Auto-pairs
   {
-    "windwp/nvim-autopairs",            -- Auto pairs for brackets, quotes, etc.
+    "windwp/nvim-autopairs",
     config = function()
       require("nvim-autopairs").setup()
-    end
+    end,
   },
 
   -- Commenting
   {
-    "numToStr/Comment.nvim",            -- Easy commenting
+    "numToStr/Comment.nvim",
     config = function()
       require("Comment").setup()
-    end
+    end,
   },
 
   -- Bufferline
   {
-    "akinsho/bufferline.nvim",          -- Bufferline (tabs)
+    "akinsho/bufferline.nvim",
     dependencies = { "moll/vim-bbye" },
     config = function()
       require("bufferline").setup({
         options = {
-          offsets = {{ filetype = "NvimTree", text = "File Explorer" }},
+          offsets = { { filetype = "NvimTree", text = "File Explorer" } },
           separator_style = "slant",
         },
       })
-    end
+    end,
   },
+
+  -- vimtex for LaTeX support
+  {
+    "lervag/vimtex",
+    ft = "tex",  -- Load only for LaTeX files
+    config = function()
+      -- vimtex configuration
+      vim.g.vimtex_view_method = "zathura"  -- Use zathura as the PDF viewer (Linux)
+      vim.g.vimtex_compiler_method = "latexmk"  -- Use latexmk for compilation
+      vim.g.vimtex_quickfix_enabled = 1  -- Enable quickfix for errors
+      vim.g.vimtex_syntax_enabled = 1  -- Enable syntax highlighting
+      vim.g.vimtex_compiler_progname = "nvim"  -- Use Neovim as the compiler progname
+    end,
+  },
+    {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+    routes = {
+      {
+        filter = {
+          event = "msg_show",
+          any = {
+            { find = "%d+L, %d+B" },
+            { find = "; after #%d+" },
+            { find = "; before #%d+" },
+          },
+        },
+        view = "mini",
+      },
+    },
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+    },
+  },
+  -- stylua: ignore
+  keys = {
+    { "<leader>sn", "", desc = "+noice"},
+    { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+    { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+    { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+    { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+    { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+    { "<leader>snt", function() require("noice").cmd("pick") end, desc = "Noice Picker (Telescope/FzfLua)" },
+    { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
+    { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
+  },
+  config = function(_, opts)
+    -- HACK: noice shows messages from before it was enabled,
+    -- but this is not ideal when Lazy is installing plugins,
+    -- so clear the messages in this case.
+    if vim.o.filetype == "lazy" then
+      vim.cmd([[messages clear]])
+    end
+    require("noice").setup(opts)
+  end,
+},
+    {
+  "echasnovski/mini.icons",
+  lazy = true,
+  opts = {
+    file = {
+      [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+      ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+    },
+    filetype = {
+      dotenv = { glyph = "", hl = "MiniIconsYellow" },
+    },
+  },
+  init = function()
+    package.preload["nvim-web-devicons"] = function()
+      require("mini.icons").mock_nvim_web_devicons()
+      return package.loaded["nvim-web-devicons"]
+    end
+  end,
+},
+    { "MunifTanjim/nui.nvim", lazy = true },
+    {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts_extend = { "spec" },
+  opts = {
+    preset = "helix",
+    defaults = {},
+    spec = {
+      {
+        mode = { "n", "v" },
+        { "<leader><tab>", group = "tabs" },
+        { "<leader>c", group = "code" },
+        { "<leader>d", group = "debug" },
+        { "<leader>dp", group = "profiler" },
+        { "<leader>f", group = "file/find" },
+        { "<leader>g", group = "git" },
+        { "<leader>gh", group = "hunks" },
+        { "<leader>q", group = "quit/session" },
+        { "<leader>s", group = "search" },
+        { "<leader>u", group = "ui", icon = { icon = "󰙵 ", color = "cyan" } },
+        { "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
+        { "[", group = "prev" },
+        { "]", group = "next" },
+        { "g", group = "goto" },
+        { "gs", group = "surround" },
+        { "z", group = "fold" },
+        {
+          "<leader>b",
+          group = "buffer",
+          expand = function()
+            return require("which-key.extras").expand.buf()
+          end,
+        },
+        {
+          "<leader>w",
+          group = "windows",
+          proxy = "<c-w>",
+          expand = function()
+            return require("which-key.extras").expand.win()
+          end,
+        },
+        -- better descriptions
+        { "gx", desc = "Open with system app" },
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Keymaps (which-key)",
+    },
+    {
+      "<c-w><space>",
+      function()
+        require("which-key").show({ keys = "<c-w>", loop = true })
+      end,
+      desc = "Window Hydra Mode (which-key)",
+    },
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+    if not vim.tbl_isempty(opts.defaults) then
+      LazyVim.warn("which-key: opts.defaults is deprecated. Please use opts.spec instead.")
+      wk.register(opts.defaults)
+    end
+  end,
+}
 })
 
 -- General settings
-vim.opt.syntax = "on"                    -- Enable syntax highlighting
-vim.opt.number = true                    -- Show line numbers
-vim.opt.relativenumber = false           -- Disable relative line numbers (set to absolute)
-vim.opt.mouse = "a"                      -- Enable mouse support
-vim.opt.tabstop = 4                      -- Set tab width to 4 spaces
-vim.opt.shiftwidth = 4                   -- Set indentation width to 4 spaces
-vim.opt.expandtab = true                 -- Convert tabs to spaces
-vim.opt.smartindent = true               -- Smart indenting
-vim.opt.wrap = false                     -- Disable line wrapping
-vim.opt.clipboard = "unnamedplus"        -- Use system clipboard
+vim.opt.syntax = "on"
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.mouse = "a"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.clipboard = "unnamedplus"
 
 -- Enable line numbers
 vim.opt.number = true
-vim.opt.relativenumber = false          -- Set relative line numbers to false
+vim.opt.relativenumber = false
 
 -- Set up auto-save on exit
 vim.api.nvim_create_autocmd("VimLeavePre", {
   callback = function()
     vim.cmd("write")
-  end
+  end,
 })
 
 -- Key mappings
-vim.keymap.set('n', '<C-space>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })  -- Toggle NvimTree
-vim.keymap.set('n', '<C-space>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })  -- Find files with Telescope
-vim.keymap.set('n', '<C-space>fg', ':Telescope live_grep<CR>', { noremap = true, silent = true })  -- Find text with Telescope
-vim.keymap.set('n', '<C-space>fb', ':Telescope buffers<CR>', { noremap = true, silent = true })  -- Find buffers with Telescope
-vim.keymap.set('n', '<C-space>fh', ':Telescope help_tags<CR>', { noremap = true, silent = true })  -- Find help tags with Telescope
+vim.keymap.set("n", "<Leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })  -- Toggle NvimTree
+vim.keymap.set("n", "<Leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true })  -- Find files with Telescope
+vim.keymap.set("n", "<Leader>fg", ":Telescope live_grep<CR>", { noremap = true, silent = true })  -- Find text with Telescope
+vim.keymap.set("n", "<Leader>fb", ":Telescope buffers<CR>", { noremap = true, silent = true })  -- Find buffers with Telescope
+vim.keymap.set("n", "<Leader>fh", ":Telescope help_tags<CR>", { noremap = true, silent = true })  -- Find help tags with Telescope
 
 -- LSP mappings
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true })  -- Go to definition
-vim.keymap.set('n', 'gr', vim.lsp.buf.references, { noremap = true, silent = true })  -- Find references
-vim.keymap.set('n', '<C-space>rn', vim.lsp.buf.rename, { noremap = true, silent = true })  -- Rename symbol
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })  -- Go to definition
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { noremap = true, silent = true })  -- Find references
+vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, { noremap = true, silent = true })  -- Rename symbol
 
 -- Bufferline mappings
-vim.keymap.set('n', '<C-space>bn', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })  -- Next buffer
-vim.keymap.set('n', '<C-space>bp', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })  -- Previous buffer
+vim.keymap.set("n", "<Leader>bn", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })  -- Next buffer
+vim.keymap.set("n", "<Leader>bp", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })  -- Previous buffer
 
+-- Image viewer mappings
+vim.keymap.set("n", "<Leader>im", ":ImageOpen<CR>", { noremap = true, silent = true })  -- Open an image
+vim.keymap.set("n", "<Leader>ir", ":ImageReload<CR>", { noremap = true, silent = true })  -- Reload the current image
+vim.keymap.set("n", "<Leader>ic", ":ImageClose<CR>", { noremap = true, silent = true })  -- Close the current image
